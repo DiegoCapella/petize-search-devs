f2b3b68c2a40727f8bd4eb2441ec323d271cb1e7
DiegoCapella <diegocapelladev@gmail.com>
2023-05-13 09:02:40 -0300

    feat: add Home page layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,21 @@
+import { Container, Grid } from '@mui/material'
+import img from '../../assets/Search d_evs.png'
+import { Search } from '../../components/Search'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <Container>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        gap={8}
+        height="100vh"
+      >
+        <img src={img} />
+        <Search />
+      </Grid>
+    </Container>
+  )
 }
